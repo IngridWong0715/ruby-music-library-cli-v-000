5a2246a270f55fcf7af6eaa09cde030caa624402
Ingrid Wong <yw340@georgetown.edu>
2018-01-23 16:26:50 +0000

    automatically backed up by learn

--- lib/concerns/persistable.rb
+++ lib/concerns/persistable.rb
@@ -2,4 +2,15 @@ module Persistable
   module ClassMethods
   end
   module InstanceMethods
+
+      def save #instance
+        @@all << self #self.class.save << self
+
+      end
+  end
+end
+
+
+  def self.destroy_all #class
+    self.all.clear
   end
