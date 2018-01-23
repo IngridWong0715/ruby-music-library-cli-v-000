3f842918f25e13cfbc3418df81ef9afd1aefd5b1
Ingrid Wong <yw340@georgetown.edu>
2018-01-23 16:28:51 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -2,6 +2,8 @@ require 'pry'
 
 class Genre
     extend Findable::ClassMethods
+    extend Persistable::ClassMethods
+    include Persistable::InstanceMethods
   attr_accessor :name, :songs
   @@all = []
   def initialize(name)
@@ -11,12 +13,7 @@ class Genre
   def self.all
     @@all
   end
-  def save
-    @@all << self
-  end
-  def self.destroy_all
-    self.all.clear
-  end
+
 
   def self.create(name)
     genre = Genre.new(name)
